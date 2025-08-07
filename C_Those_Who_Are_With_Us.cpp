c7f8aed6d3dd652d060911e8478ef1b9086e86b4
Ahsanul Imam Ovi <ahsanulimam10923@gmail.com>
2025-08-07 20:02:59 +0600

    contest 24

---
+++ C_Those_Who_Are_With_Us.cpp
@@ -0,0 +1,148 @@
+// IN THE NAME OF ALLAH
+// Coded by Ahsanul Imam
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>order_set;
+// find_by_order() - Returns an iterator to the k-th largest element(counting from zero)
+// order_of_key() - The number of items in a set that are strictly smaller than our item
+typedef long long int ll;
+const int  MOD = 1000000007;
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define f first
+#define s second
+#define nl '\n';
+#define bit(a) __builtin_popcountll(a)
+#define cnt_sese_sunno(x) __builtin_ctz(x)
+#define cnt_age_sunno(x) __builtin_clz(x)
+#define cnt_sese_ek(x) __builtin_ctz(~x)
+#define cnt_age_ek(x) __builtin_clz(~x)
+#define parity(x) __builtin_parity(x)
+#define parityLL(x) __builtin_parityll(x)
+#define w(x) while (x--)
+#define test int t; cin>>t; for (int i = 1; i <= t; i++)
+#define loop for (int i = 0; i < n; i++)
+#define print(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define gcd(a, b) __gcd<long long int>(a, b)
+#define lcm(a, b) (a * b) / gcd(a, b)
+#define pi 2*acos(0.0)
+#define pr pair<int, int>
+#define Ahsanul (ios_base::sync_with_stdio(false), cin.tie(NULL))
+vector<pr>knight_posible{{2, 1}, {2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}};//knight moves
+vector<pr>posible{{0,-1},{0,1},{-1,0},{1,0},{-1,-1},{-1,1},{1,-1},{1,1}};//eight direction moves;
+int power(int a,int b){int res=1;while (b){if (b&1){res=(res*1ll*a)%MOD;}a=(a*1ll*a)%MOD;b>>=1;}return res;}
+ll inversemod(ll base) { return power(base,MOD-2); }
+void solve()
+{
+    int n,m; cin>>n>>m;
+    int v[n+1][m+1];
+    int maxi=0;
+    vector<pr>fre;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            cin>>v[i][j];
+            maxi=max(maxi,v[i][j]);
+        }
+    }
+    vi row(n+1),col(m+1);
+    int cnt=0;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            if(v[i][j]==maxi)
+            {
+                row[i]++;
+                col[j]++;
+                cnt++;
+            }
+        }
+    }
+    int max_cnt=0,idx=-1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            if(v[i][j]==maxi)
+            {
+                if(row[i]+col[j]-1==cnt)
+                {
+                    cout<<maxi-1<<nl;
+                    return;
+                }
+            }
+            else if(row[i]+col[j]==cnt)
+            {
+                cout<<maxi-1<<nl;
+                return;
+            }
+        }
+    
+    }
+    cout<<maxi<<nl;
+    // //cout<<idx<<nl;
+    // bool vis[n+1][m+1]={};
+    // vector<pr>col;
+    // for(auto &[x,y]:fre)
+    // {
+    //     if(y==idx and !vis[x][y])
+    //     {
+    //         vis[x][y]=true;
+    //         col.pb({x,y});
+    //     }
+    // }
+    // vector<pr>row;
+    // for(auto &[x,y]:fre)
+    // {
+    //     if(!vis[x][y])
+    //     {
+    //         if(row.empty())
+    //         {
+    //             row.pb({x,y});
+    //             vis[x][y]=true;
+    //         }
+    //         else
+    //         {
+    //             int row_idx=row[0].f;
+    //             if(x==row_idx)
+    //             {
+    //                 row.pb({x,y});
+    //                 vis[x][y]=true;
+    //             }
+    //             else
+    //             {
+    //                 //cout<<x<<" "<<row_idx<<nl;
+    //                 cout<<maxi<<nl;
+    //                 return;
+    //             }
+    //         }
+    //     }
+    // }
+    // if((int)col.size()+(int)row.size()==(int)fre.size())
+    // {
+    //     cout<<maxi-1<<nl;
+    //     return;
+    // }
+    // else
+    // {
+    //     cout<<maxi<<nl;
+    //     return;
+    // }
+}
+int main()
+{
+    Ahsanul;
+    test
+    {
+        //cout<<"Case "<<i<<": ";
+        solve();
+    }
+}
